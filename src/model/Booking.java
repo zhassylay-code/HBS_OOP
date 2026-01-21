3390f3e5d0a04a7625a6777c0e428b4800bfbced
dianabakyt <dianabakyt190107@gmail.com>
2026-01-21 16:43:46 +0500

    booking commit

---
+++ src/model/Booking.java
@@ -0,0 +1,9 @@
+package model;
+import java.time.LocalDate;
+public class Booking {
+    public int roomId;
+    public int guestId;
+    public LocalDate startDate;
+    public LocalDate endDate;
+    public double totalPrice;
+}
