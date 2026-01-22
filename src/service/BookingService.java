7faed51801cc4d5c6e70055bc5108fa5ce6458f3
Sanzhar <sanzhar06859@gmail.com>
2026-01-22 23:06:56 +0500

    importing everything in service

--- src/service/BookingService.java
+++ src/service/BookingService.java
@@ -1,4 +1,15 @@
-package service;
+package service; //Business logic
+
+import entity.Booking;
+import entity.Room;
+import exception.InvalidBookingDatesException;
+import exception.RoomAlreadyBookedException;
+import repository.BookingRepository;
+import repository.RoomRepository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit; //used to calculate number of days between dates(java)
 
 public class BookingService {
 
