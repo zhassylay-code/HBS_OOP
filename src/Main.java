5f422ca5aae23522343f982c0414c85a2084ebbb
asssylay.zh <zh.assylay@gmail.com>
2026-01-22 23:02:02 +0500

    first commit: greeting

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import UI.ConsoleMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleMenu menu = new ConsoleMenu();
+        menu.start();
+    }
+}
---
+++ src/UI/ConsoleMenu.java
@@ -0,0 +1,8 @@
+package UI;
+
+public class ConsoleMenu {
+
+    public void start() {
+    System.out.println("Welcome to the hotel! How can we help you?");
+    }
+}
