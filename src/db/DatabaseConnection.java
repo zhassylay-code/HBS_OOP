1ffdfde209b2668a30ac9c0a1f45c86925190f60
dianabakyt <dianabakyt190107@gmail.com>
2026-01-23 17:19:23 +0500

    added pomxml

--- src/db/DatabaseConnection.java
+++ src/db/DatabaseConnection.java
@@ -2,19 +2,21 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
 
     private static final String URL =
-            "jdbc:postgresql://localhost:5432/hotel_db";
+            "jdbc:postgresql://localhost:5432/hotel_booking_db";
     private static final String USER = "postgres";
     private static final String PASSWORD = "0000";
 
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 }
+
