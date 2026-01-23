b177877bef28f9afa6d92be71925a8027b8d73fa
dianabakyt <dianabakyt190107@gmail.com>
2026-01-23 19:47:03 +0500

    room commit

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,64 +1,26 @@
 package model;
 public class Room {
     private int id;
+    private String roomNumber;
     private String type;
-    private int capacity;
-    private double price;
-    private boolean isAvailable;
+    private double pricePerNight;
 
-    public Room() {}
-
-    public Room(String type, int capacity, double price, boolean isAvailable) {
+    public Room(int id, String roomNumber, String type, double pricePerNight) {
+        this.id = id;
+        this.roomNumber = roomNumber;
         this.type = type;
-        this.capacity = capacity;
-        this.price = price;
-        this.isAvailable = isAvailable;
+        this.pricePerNight = pricePerNight;
     }
-
-
     public int getId() {
         return id;
     }
-    public void setId(int id) {
-        this.id = id;
+    public String getRoomNumber() {
+        return roomNumber;
     }
-
     public String getType() {
         return type;
     }
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-    public void setAvailable(boolean available) {
-        isAvailable = available;
-    }
-
-    @Override
-    public String toString() {
-        return "Room{" +
-                "id=" + id +
-                ", type='" + type + '\'' +
-                ", capacity=" + capacity +
-                ", price=" + price +
-                ", isAvailable=" + isAvailable +
-                '}';
+    public double getPricePerNight() {
+        return pricePerNight;
     }
 }
