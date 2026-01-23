c26765d344aafee4ea6a0a438cecf623097721fc
dianabakyt <dianabakyt190107@gmail.com>
2026-01-23 19:46:32 +0500

    booking commit

--- src/model/Booking.java
+++ src/model/Booking.java
@@ -1,62 +1,39 @@
 package model;
 import java.time.LocalDate;
 public class Booking {
+
     private int id;
     private int roomId;
     private int guestId;
     private LocalDate startDate;
     private LocalDate endDate;
+    private double totalPrice;
 
-    public Booking() {}
-
-    public Booking(int roomId, int guestId, LocalDate startDate, LocalDate endDate) {
+    public Booking(int id, int roomId, int guestId, LocalDate startDate, LocalDate endDate,
+                   double totalPrice) {
+        this.id = id;
         this.roomId = roomId;
         this.guestId = guestId;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.totalPrice = totalPrice;
     }
-
     public int getId() {
         return id;
     }
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public int getRoomId() {
         return roomId;
     }
-    public void setRoomId(int roomId) {
-        this.roomId = roomId;
-    }
-
     public int getGuestId() {
         return guestId;
-    }
-    public void setGuestId(int guestId) {
-        this.guestId = guestId;
     }
     public LocalDate getStartDate() {
         return startDate;
     }
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
     public LocalDate getEndDate() {
         return endDate;
     }
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Booking{" +
-                "id=" + id +
-                ", roomId=" + roomId +
-                ", guestId=" + guestId +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
-                '}';
+    public double getTotalPrice() {
+        return totalPrice;
     }
 }
