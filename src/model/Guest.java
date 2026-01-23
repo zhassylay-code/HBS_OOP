5fd26a4422c1bdf8cb8f5e9ea16d95078b7141b1
dianabakyt <dianabakyt190107@gmail.com>
2026-01-23 19:46:51 +0500

    guest commit

--- src/model/Guest.java
+++ src/model/Guest.java
@@ -1,54 +1,22 @@
 package model;
 public class Guest {
     private int id;
-    private String name;
-    private String email;
+    private String fullName;
     private String phone;
 
-    public Guest() {
-    }
-
-    public Guest(String name, String email, String phone) {
-        this.name = name;
-        this.email = email;
+    public Guest(int id, String fullName, String phone) {
+        this.id = id;
+        this.fullName = fullName;
         this.phone = phone;
     }
-
-
     public int getId() {
         return id;
     }
-    public void setId(int id) {
-        this.id = id;
+    public String getFullName() {
+        return fullName;
     }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPhone() {
         return phone;
     }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "Guest{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                '}';
-    }
 }
+
